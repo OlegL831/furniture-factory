d19a7ea85b5c444a7dd76b55d29375b1f063850f
OlegL831 <starko.831@mail.ru>
2022-02-23 21:35:35 +0300

    section forms + carouselmin

---
+++ src/js/style.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.carouselmin_carousel').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/carouselmin/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/carouselmin/right.png"></button>',
+    })
+})
