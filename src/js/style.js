46b0b9ffa4342792537fec8096c82bd2c734753a
OlegL831 <starko.831@mail.ru>
2022-02-25 22:31:14 +0300

    section partners

--- src/js/style.js
+++ src/js/style.js
@@ -8,3 +8,14 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="img/carouselmin/right.png"></button>',
     })
 })
+
+$(document).ready(function(){
+    $('.partners_carousel').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/partners/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/partners/right.png"></button>',
+    })
+})
